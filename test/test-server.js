e07de7a495374b3ac2b55c3b375bc0ca5cc7ae31
Sakthivel <98067020+sramakris@users.noreply.github.com>
2022-11-04 14:58:27 -0400

    Update test-server.js

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>Welcome to [TEAM NAME'S] application</h1>"));
+        chai.assert.isFalse(res.text.includes("<h1>Welcome to [TEAM NAME'S] application</h1>"));
         return done();
       });
 
